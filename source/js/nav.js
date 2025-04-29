521ae9e64436a13b4341f18f80f1a595ac90f4da
Daria <dashusik_@mail.ru>
2025-04-29 16:49:41 +0300

    FIX(header): nav.js and styles

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,12 +1,30 @@
-const elementNav = document.querySelector('.header');
+const elementNav = document.querySelector('.header__wrapper');
 const elementToggle = document.querySelector('.header__toggle');
+const navItems = document.querySelectorAll('.header__nav-item');
+const overlay = document.querySelector('.overlay');
 
-elementToggle.addEventListener('click', function() {
-    if (elementNav.classList.contains('header--closed')) {
+const closeMenu = () => {
+  elementNav.classList.add('header--closed');
+  elementNav.classList.remove('header--opened');
+  overlay.style.display = 'none';
+  document.body.style.overflow = '';
+};
+
+const toggleMenu = () => {
+  if (elementNav.classList.contains('header--closed')) {
     elementNav.classList.remove('header--closed');
     elementNav.classList.add('header--opened');
+    overlay.style.display = 'block';
+    document.body.style.overflow = 'hidden';
   } else {
-    elementNav.classList.add('header--closed');
-    elementNav.classList.remove('header--opened');
+    closeMenu();
   }
+};
+
+elementToggle.addEventListener('click', toggleMenu);
+
+navItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    closeMenu();
+  });
 });
