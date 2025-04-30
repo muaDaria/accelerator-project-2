d2c2c1d56e4376d639e581a81c5a31bda6176c9b
Daria <dashusik_@mail.ru>
2025-04-30 16:35:02 +0300

    FIX(trainers): styles

--- source/js/trainers-slider.js
+++ source/js/trainers-slider.js
@@ -1,6 +1,5 @@
 import Swiper from 'swiper';
 import {Navigation} from 'swiper/modules';
-//import 'swiper/css';
 
 const initSliderTrainers = () => {
   const sliderElement = document.querySelector('.trainers__swiper');
@@ -12,55 +11,38 @@ const initSliderTrainers = () => {
     modules: [Navigation],
 
     direction: 'horizontal',
-    loop: true,
+    loop: false,
     grabCursor: true,
+    initialSlide: 2,
     slidesPerView: 1,
-    //autoHeight: true,
-    //initialSlide: 0,
     simulateTouch: true,
-    effect: 'fade',
-
-    fadeEffect: {
-      crossFade: true,
-    },
-
-    //watchOverflow: true,
-    //centeredSides: false,
+    allowTouchMove: true,
 
     navigation: {
       nextEl: '.trainers__button-next',
       prevEl: '.trainers__button-prev',
     },
 
-    //mousewheel: {
-    // forceToAxis: true,
-    // releaseOnEdges: true,
-    //},
-
     breakpoints: {
       320: {
         slidesPerView: 1,
         initialSlide: 2,
         simulateTouch: true,
+        allowTouchMove: true,
       },
       768: {
         slidesPerView: 3,
         spaceBetween: 20,
-        simulateTouch: false,
+        simulateTouch: true,
+        allowTouchMove: true,
       },
       1440: {
         slidesPerView: 4,
         spaceBetween: 20,
         simulateTouch: false,
-      }
-    }
-    /*
-
-    breakpoints: {
-      1440: {
         allowTouchMove: false,
       }
-    }*/
+    }
   });
 };
 
