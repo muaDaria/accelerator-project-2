add1243e399302c3a54cf4e447d1937b1517a3f1
Daria <dashusik_@mail.ru>
2025-05-02 11:52:01 +0300

    REFACTOR(header): nav

--- source/js/form.js
+++ source/js/form.js
@@ -1,50 +1,63 @@
-const form = document.querySelector('.form__group');
-const fieldName = form.querySelector('.form__field--name');
-const fieldPhone = form.querySelector('.form__field--phone');
-const errorTextName = form.querySelector('.form__error--name');
-const errorTextPhone = form.querySelector('.form__error--phone');
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.form__group');
+  const fieldEmail = form.querySelector('.form__field--email');
+  const fieldPhone = form.querySelector('.form__field--phone');
 
-function formIsValid() {
-  errorTextName.classList.add('visually-hidden');
-  errorTextPhone.classList.add('visually-hidden');
-
-  const name = fieldName.value;
-  const phone = fieldPhone.value;
-
-  let isValid = true;
-
-  if (!isValidName(name)) {
-    fieldName.style.borderBottom = '1px solid #ff121f';
-    errorTextName.textContent = 'Введите корректное имя';
-    errorTextName.style.color = '#ff121f';
-    errorTextName.classList.remove('visually-hidden');
-    isValid = false;
+  function validateEmail(email) {
+    const pattern = /^[\wа-яА-ЯёЁ._%+-]+@([\wа-яА-ЯёЁ-]+\.)+[a-zA-Zа-яА-ЯёЁ]{2,}$/;
+    return pattern.test(email);
   }
 
-  if (!isValidPhone(phone)) {
-    fieldPhone.style.borderBottom = '1px solid #ff121f';
-    errorTextPhone.textContent = 'Введите корректный номер телефона';
-    errorTextPhone.style.color = '#ff121f';
-    errorTextPhone.classList.remove('visually-hidden');
-    isValid = false;
+  function validatePhone(phone) {
+    const pattern = /^((\+7)|(8))[0-9]{10}$/;
+    return pattern.test(phone);
   }
 
-  return isValid;
-}
-
-function isValidName(name) {
-  const pattern = /^([A-Za-zА-Яа-яЁё]+$)|([A-Za-zА-Яа-яЁё]+\s[A-Za-zА-Яа-яЁё]+$)/;
-  return pattern.test(name);
-}
-
-function isValidPhone(phone) {
-  const pattern = /^((\+7)|(8))[0-9]{10}$/;
-  return pattern.test(phone);
-}
-
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  if (formIsValid()) {
-    form.submit();
+  function validateField(field, validateFn, emptyMessage, invalidMessage) {
+    if (!field.value.trim()) {
+      field.setCustomValidity(emptyMessage);
+      field.classList.add('form__field-error');
+      return false;
+    } else if (!validateFn(field.value)) {
+      field.setCustomValidity(invalidMessage);
+      field.classList.add('form__field-error');
+      return false;
+    } else {
+      field.setCustomValidity('');
+      field.classList.remove('form__field-error');
+      return true;
+    }
   }
+
+  [fieldEmail, fieldPhone].forEach((field) => {
+    field.addEventListener('input', () => {
+      field.setCustomValidity('');
+      field.classList.remove('form__field-error');
+    });
+  });
+
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    let isValid = true;
+
+    if (!validateField(fieldEmail, validateEmail,
+      'Поле Email обязательно для заполнения.',
+      'Введите корректный email адрес.')) {
+      isValid = false;
+    }
+
+    if (!validateField(fieldPhone, validatePhone,
+      'Поле Телефон обязательно для заполнения.',
+      'Введите корректный номер телефона.')) {
+      isValid = false;
+    }
+
+    if (isValid) {
+      form.submit();
+    } else {
+      fieldEmail.reportValidity();
+      fieldPhone.reportValidity();
+    }
+  });
 });
